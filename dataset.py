eeb3ff18ed65646d84bd1e3801a458b43110ebde
zayn7lie-win <zayn7lie.ber7+github@gmail.com>
2023-02-22 11:39:55 +0800

    resnet-50 setting

--- dataset.py
+++ dataset.py
@@ -1,3 +1,4 @@
+from torch import Tensor
 from torch.utils.data import Dataset
 from torchvision import transforms
 
@@ -26,17 +27,22 @@ class odirData(Dataset):
         self.img_folder = self.path + "/Images"
         for i in range(label.shape[0]):
             self.filenames.append(label[i][0])
-            self.labels.append([label[i][1], label[i][2], label[i][3], label[i][4], label[i][5], label[i][6], label[i][7], label[i][8],])
+            self.labels.append([float(label[i][1]), float(label[i][2]), float(label[i][3]), float(label[i][4]), float(label[i][5]), float(label[i][6]), float(label[i][7]), float(label[i][8])])
             # 1N 2D 3G 4C 5A 6H 7M 8O
 
     def __getitem__(self, index):
         img_name = self.img_folder + "/" + self.filenames[index]
-        label = self.labels[index]
-        
-        # img = plt.imread(img_name)
-        img = Image.open(img_name).convert('RGB')
-        img = self.transform(img)
+        label = []
+        # print(self.filenames[index])
+        # print(self.labels[index])
+        # for i in range(0, 8):
+            # label.append(tensor(self.labels[index][i])) # self.labels[index][i]
+        label = np.array(self.labels[index], dtype=float)
 
+        # img = plt.imread(img_name)
+        img = Image.open(img_name)
+        img = self.transform(img)
+        # print(label)
         return img, label
 
     def __len__(self):
--- main.py
+++ main.py
@@ -9,18 +9,66 @@ print(os.listdir(os.getcwd()))
 #'''
 
 from dataset import odirData
-import model
+import torch
 from torch.utils.data import DataLoader
-import matplotlib.pyplot as plt
-import numpy as np
+from torch import nn
+from torch import optim
+from torch.autograd import Variable
 import torchvision
+
+# para setting
+modellr = 1e-4
+BATCH_SIZE = 20
+EPOCHS = 20
+DEVICE = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+# model create
+criterion = nn.BCELoss()
+model = torchvision.models.resnet50()
+num_ftrs = model.fc.in_features
+model.fc = nn.Linear(num_ftrs, 8)
+model.to(DEVICE)
+optimizer = optim.Adam(model.parameters(), lr=modellr)
+
+# load data
+train_dataset = odirData("./OIA-ODIR/Training Set")
+test_dataset = odirData("./OIA-ODIR/Off-site Test Set")
+train_loader = DataLoader(train_dataset, BATCH_SIZE, shuffle=True)
+test_loader = DataLoader(test_dataset, BATCH_SIZE, shuffle=True)
+
+def lrAdjust(optimizer, epoch):
+    """Sets the learning rate to the initial LR decayed by 10 every 30 epochs"""
+    modellrnew = modellr * (0.1 ** (epoch // 50))
+    print("lr:", modellrnew)
+    for param_group in optimizer.param_groups:
+        param_group['lr'] = modellrnew
+
+def train(model, device, train_loader, optimizer, epoch):
+    model.train()
+    sum_loss = 0
+    total_num = len(train_loader.dataset)
+    print(total_num, len(train_loader))
+    for batch_idx, (data, targets) in enumerate(train_loader):
+        data = Variable(data).to(device)
+        output = model(data)
+        loss = criterion(output, targets.type(torch.float)) # criterion = nn.BCELoss()
+        loss.backward()
+        optimizer.zero_grad()
+        optimizer.step()
+        print_loss = loss.data.item()
+        sum_loss += print_loss
+        if (batch_idx + 1) % 50 == 0:
+            print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
+                epoch, (batch_idx + 1) * len(data), len(train_loader.dataset),
+                       100. * (batch_idx + 1) / len(train_loader), loss.item()))
+    ave_loss = sum_loss / len(train_loader)
+    print('epoch:{},loss:{}'.format(epoch, ave_loss))
+
 def main():
-    # load data
-    train_dataset = odirData("./OIA-ODIR/Training Set")
-    test_dataset = odirData("./OIA-ODIR/Off-site Test Set")
-    train_loader = DataLoader(train_dataset, batch_size=8, shuffle=True)
-    test_loader = DataLoader(test_dataset, batch_size=8, shuffle=True)
-    print(train_dataset.__getitem__(1))
+    for epoch in range(1, EPOCHS + 1):
+        lrAdjust(optimizer, epoch)
+        train(model, DEVICE, train_loader, optimizer, epoch)
+    return 0
 
     
 if __name__ == "__main__":
