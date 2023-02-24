f19d8f06c0857119c1e66eb6dcd8cbe8414fb6b2
zayn7lie-win <zayn7lie.ber7+github@gmail.com>
2023-02-24 22:21:17 +0800

    test

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ def main():
     # para setting
     modellr = 1e-4
     BATCH_SIZE = 72
-    EPOCHS = 1 # 50
+    EPOCHS = 50 # 50
     DEVICE = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
     # load data
