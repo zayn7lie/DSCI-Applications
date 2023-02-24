6a37c1674c1eb12dbfb74c23edc4e9d479516d82
zayn7lie-win <zayn7lie.ber7+github@gmail.com>
2023-02-24 12:02:11 +0800

    create RMMD

--- model.py
+++ model.py
@@ -1,7 +1,17 @@
-import torch
 from torch import nn
 from torchvision import models
 class Resnet50(nn.Module):
+    def __init__(self, n_classes):
+        super().__init__()
+        resnet = models.resnet50()
+        resnet.fc = nn.Linear(in_features=resnet.fc.in_features, out_features=8)
+        self.base_model = resnet
+        self.sigm = nn.Sigmoid()
+
+    def forward(self, x):
+        return self.sigm(self.base_model(x))
+
+class RMMD(nn.Module):
     def __init__(self, n_classes):
         super().__init__()
         resnet = models.resnet50()
