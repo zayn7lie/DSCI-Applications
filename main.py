ba5cf80ee841b47323e0db6614f5ec9d3ca9bc5a
zayn7lie-win <zayn7lie.ber7+github@gmail.com>
2023-02-24 10:44:47 +0800

    val create

--- main.py
+++ main.py
@@ -67,10 +67,46 @@ def train(model, device, train_loader, optimizer, epoch):
     ave_loss = sum_loss / len(train_loader)
     print('epoch:{},loss:{}'.format(epoch, ave_loss))
 
+from sklearn.metrics import cohen_kappa_score
+from sklearn.metrics import f1_score
+from sklearn.metrics import roc_auc_score
+
+def eval(model, device, test_loader):
+    model.eval()
+    test_loss = 0
+    kappa = 0
+    f1 = 0
+    auc = 0
+    total_num = len(test_loader.dataset)
+    print(total_num, len(test_loader))
+    with torch.no_grad():
+        for imgs, targets in test_loader:
+            imgs, targets = imgs.to(device), targets.to(device)
+
+            output = model(imgs)
+            loss = criterion(output, targets.type(torch.float))
+            
+            print_loss = loss.data.item()
+            test_loss += print_loss
+
+            output, targets = output.cpu().numpy(), targets.cpu().numpy()
+            for i in range(output):
+                if output[i] >= 0.5: output[i] = 1
+                else: output[i] = 0
+            kappa += cohen_kappa_score(targets, output)
+            f1 += f1_score(targets, output)
+            auc += roc_auc_score(targets, output)
+        avgloss = 1.000 * test_loss / len(test_loader)
+        avgkappa = 1.000 * kappa / len(test_loader)
+        avgf1 = 1.000 * f1 / len(test_loader)
+        avgauc = 1.000 * auc / len(test_loader)
+        print('\nVal set: Average loss: {:.4f} Average kappa: {:.4f} Average f1: {:.4f} Average auc: {:.4f}\n', avgloss, avgkappa, avgf1, avgauc)
+
 def main():
     for epoch in range(1, EPOCHS + 1):
         lrAdjust(optimizer, epoch)
         train(model, DEVICE, train_loader, optimizer, epoch)
+        eval(model, DEVICE, test_loader)
     return 0
 
     
