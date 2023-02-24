9497aaca51e16de22bad65e1abfb56ba52527b53
zayn7lie-win <zayn7lie.ber7+github@gmail.com>
2023-02-24 17:24:14 +0800

    print update

--- main.py
+++ main.py
@@ -35,7 +35,6 @@ def main():
         for epoch in range(1, EPOCHS + 1):
             adjust_lr(optimizer, epoch, modellr)
             train(model, DEVICE, train_loader, optimizer, epoch)
-            adjust_lr(optimizer, epoch + 1, modellr)
         torch.save(model.state_dict(), "./modelCache")
         
     
