195428a597a0d1479a1baac41db79a6124ae3529
zayn7lie-win <zayn7lie.ber7+github@gmail.com>
2023-02-24 18:10:56 +0800

    para update

--- main.py
+++ main.py
@@ -11,9 +11,9 @@ def main():
     # workDirChanger("C:/Users/zayn7lie/OneDrive - ber7/02-Prog/GitHub/ML-RMMD")
 
     # para setting
-    modellr = 1e-4
-    BATCH_SIZE = 100
-    EPOCHS = 50 # 50
+    modellr = 5 * 1e-4
+    BATCH_SIZE = 125
+    EPOCHS = 25 # 50
     DEVICE = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
     # load data
