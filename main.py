53ed186c6e7d29fd04c047ab77260080cdec6fa1
zayn7lie-win <zayn7lie.ber7+github@gmail.com>
2023-02-24 11:57:09 +0800

    fit to normal evir

--- main.py
+++ main.py
@@ -8,13 +8,13 @@ from tools import workDirChanger
 import os
 
 def main():
-    workDirChanger("C:/Users/zayn7lie/OneDrive - ber7/02-Prog/GitHub/ML-RMMD")
+    # workDirChanger("C:/Users/zayn7lie/OneDrive - ber7/02-Prog/GitHub/ML-RMMD")
 
     # para setting
     modellr = 1e-4
     BATCH_SIZE = 20
     EPOCHS = 30
-    DEVICE = torch.device('cpu')#'cuda')
+    DEVICE = torch.device('cuda') #'cpu'
 
     # load data
     train_dataset = odirData("./OIA-ODIR/Training Set")
