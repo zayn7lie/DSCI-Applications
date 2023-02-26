c8955710c38094a9d40e9669550b72df92af2813
zayn7lie-win <zayn7lie.ber7+github@gmail.com>
2023-02-26 17:36:57 +0800

    bug

--- model.py
+++ model.py
@@ -28,7 +28,7 @@ class RMMD(Resnet50):
     def __init__(self):
         super().__init__()
         self.bottleneck = nn.Sequential(
-            nn.Linear(716800 * 14, 256),
+            nn.Linear(716800, 256),
             nn.ReLU(inplace=False)
         )
     def forward(self, x, y):
