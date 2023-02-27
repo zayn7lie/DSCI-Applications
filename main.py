26dab8be205ab9f5ed39437040cc78d8e908ecb2
zayn7lie-win <zayn7lie.ber7+github@gmail.com>
2023-02-27 16:25:29 +0800

    para

--- main.py
+++ main.py
@@ -12,8 +12,8 @@ import numpy as np
 def main():
     # para setting
     modellr = 1e-4
-    BATCH_SIZE = 50 # 50
-    NUM_WORKERS = 1 # 8
+    BATCH_SIZE = 25 # 50
+    NUM_WORKERS = 2 # 8
     EPOCHS = 50 #50
     DEVICE = torch.device('cuda' if torch.cuda.is_available() else 'cpu') # xm.xla_device() # 
 
