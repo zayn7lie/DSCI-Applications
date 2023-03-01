7526da672658087d636674774d1f30b0bae0ba25
zayn7lie-win <zayn7lie.ber7+github@gmail.com>
2023-03-02 06:36:06 +0800

    test

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ import os
 def main():
     # para setting
     modellr = 1e-4
-    BATCH_SIZE = 10
+    BATCH_SIZE = 20
     NUM_WORKERS = 1 
     EPOCHS = 50
     DEVICE = torch.device('cuda' if torch.cuda.is_available() else 'cpu') # xm.xla_device()
@@ -41,7 +41,7 @@ def main():
         ts_loader_x = DataLoader(fr_dataset, BATCH_SIZE, num_workers=NUM_WORKERS, sampler=fr_ts_idxs)
         ts_loader_y = DataLoader(to_dataset, BATCH_SIZE, num_workers=NUM_WORKERS, sampler=to_ts_idxs)
         # print("K-fold:", fr_idx_9, "+", to_idx_9, "->", to_idx_1)
-        for i in [0.0000, 0.0250, 0.0500, 0.0750, 0.1000]:
+        for i in [0.0000, 0.0250, 0.0500, 0.1000, 0.2500, 0.5000, 1.0000]:
             ld = i
             # load model
             print("\nLAMBDA = {:.4f}\n".format(ld))
@@ -54,7 +54,7 @@ def main():
             optimizer = optim.Adam(model.parameters(), lr=modellr)
             for epoch in range(EPOCHS):
                 adjust_lr(optimizer, epoch, modellr)
-                train(epoch + 1, model, DEVICE, ts_loader_x, ts_loader_y, optimizer, ld)
+                train(epoch + 1, model, DEVICE, tr_loader_x, tr_loader_y, optimizer, ld)
             torch.save(model.state_dict(), "./modelCache_{:.0f}.pt".format(ld * 10000))
             
             # evaluate model
