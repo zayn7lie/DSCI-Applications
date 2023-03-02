985ab059da97fffa0c88deb87b3475223c8b4220
zayn7lie-win <zayn7lie.ber7+github@gmail.com>
2023-03-02 22:41:00 +0800

    test

--- model.py
+++ model.py
@@ -88,7 +88,7 @@ class DropBlock2D(nn.Module):
 
 class FC(nn.Module):
     def __init__(self, gamma_neg=2, gamma_pos=2, clip=0, eps=1e-8, disable_torch_grad_focal_loss=True):
-        super(AsymmetricLoss, self).__init__()
+        super(FC, self).__init__()
 
         self.gamma_neg = gamma_neg
         self.gamma_pos = gamma_pos
