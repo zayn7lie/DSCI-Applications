de6268bbdab5b38c8f0f20d395af39f0f06bb621
zayn7lie-win <zayn7lie.ber7+github@gmail.com>
2023-03-03 08:02:56 +0800

    test

--- train_eval.py
+++ train_eval.py
@@ -76,9 +76,9 @@ def eval(model, device, test_loader):
             output = np.array(output >= 0.5, dtype=float)
             # print(output)
             
-            reshape_t.append(targets)
-            reshape_o.append(output)
-
+            reshape_t = np.vstack(reshape_t, targets)
+            reshape_o = np.vstack(reshape_o, targets)
+            print(reshape_t)
             cnt += 1
         reshape_t = np.transpose(reshape_t)
         reshape_o = np.transpose(reshape_o)
