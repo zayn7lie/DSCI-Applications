33b0edb0c9953126cc8ca3a80b51a66c8254e393
zayn7lie-win <zayn7lie.ber7+github@gmail.com>
2023-03-05 07:00:56 +0800

    test

--- main.py
+++ main.py
@@ -14,8 +14,8 @@ import os
 def main():
     # para setting
     modellr = 1e-4
-    BATCH_SIZE = 10
-    NUM_WORKERS = 1 
+    BATCH_SIZE = 20
+    NUM_WORKERS = 2
     EPOCHS = 50
     DEVICE = torch.device('cuda' if torch.cuda.is_available() else 'cpu') # xm.xla_device()
     K = 10 # k-fold
