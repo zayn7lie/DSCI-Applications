3f28420914f8506933e2e04cd0744ff26980f191
zayn7lie-win <zayn7lie.ber7+github@gmail.com>
2023-03-05 06:50:23 +0800

    test

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ import os
 def main():
     # para setting
     modellr = 1e-4
-    BATCH_SIZE = 20
+    BATCH_SIZE = 30
     NUM_WORKERS = 1 
     EPOCHS = 50
     DEVICE = torch.device('cuda' if torch.cuda.is_available() else 'cpu') # xm.xla_device()
