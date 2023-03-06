caf374f872ad0c348184f74d72d90f3417004750
zayn7lie-win <zayn7lie.ber7+github@gmail.com>
2023-03-06 08:56:01 +0800

    updated

--- main.py
+++ main.py
@@ -14,9 +14,9 @@ import os
 def main():
     # para setting
     modellr = 1e-4
-    BATCH_SIZE = 60
+    BATCH_SIZE = 20
     NUM_WORKERS = 1
-    EPOCHS = 50
+    EPOCHS = 60
     DEVICE = torch.device('cuda' if torch.cuda.is_available() else 'cpu') # xm.xla_device()
     K = 10 # k-fold
     criterion = BCEFocalLosswithLogits() # torch.nn.BCEWithLogitsLoss()
