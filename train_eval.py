60692cf584f791de5343ad9c67d14cdb0e1e184e
zayn7lie-win <zayn7lie.ber7+github@gmail.com>
2023-03-06 20:50:08 +0800

    test

--- train_eval.py
+++ train_eval.py
@@ -73,11 +73,10 @@ def eval(model, device, test_loader):
         sum_t = np.transpose(sum_t)
 
         f1, auc = 0, 0
-        TH = [0.5, 0.5, 0.25, 0.25, 0.15, 0.05, 0.15, 0.5]
         for i in range(8):
-            print(sum_t[i])
-            print(np.array(sum_o[i] >= TH[i], dtype=float))
-            temp_f1, temp_auc = f1_score(sum_t[i], np.array(sum_o[i] >= TH[i], dtype=float)), roc_auc_score(sum_t[i], sum_o[i])
+            # print(sum_t[i])
+            # print(np.array(sum_o[i] >= , dtype=float))
+            temp_f1, temp_auc = f1_score(sum_t[i], np.array(sum_o[i] >= 0.45, dtype=float)), roc_auc_score(sum_t[i], sum_o[i])
             print("- {:.0f}th: F1: {:.4f}% Auc: {:.4f}%".format(i, temp_f1, temp_auc))
             f1 += temp_f1
             auc += temp_auc
