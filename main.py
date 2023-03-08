ac6221eaa6d3479cc6da67d20a29164caad254f3
zayn7lie-win <zayn7lie.ber7+github@gmail.com>
2023-03-08 13:41:57 +0800

    test

--- main.py
+++ main.py
@@ -43,9 +43,9 @@ def main():
         ts_loader_x = DataLoader(fr_dataset, BATCH_SIZE, num_workers=NUM_WORKERS, sampler=fr_ts_idxs)
         ts_loader_y = DataLoader(to_dataset, BATCH_SIZE, num_workers=NUM_WORKERS, sampler=to_ts_idxs)
         # print("K-fold:", fr_idx_9, "+", to_idx_9, "->", to_idx_1)
-        for ld in [1e-9, 0]:
+        for ld in [1e-14, 0]:
             # load model
-            print("\n### LAMBDA = {:.0f} * 1e-7\n".format(ld * 1e7))
+            print("\n### LAMBDA = {:.0f} * 1e-9\n".format(ld * 1e14))
             model = RMMD()
             model.to(DEVICE)
             if os.path.exists("./modelCache.pt"):
@@ -56,7 +56,7 @@ def main():
             for epoch in range(EPOCHS):
                 adjust_lr(optimizer, epoch, modellr)
                 train(epoch + 1, model, DEVICE, tr_loader_x, tr_loader_y, optimizer, criterion, ld)
-            torch.save(model.state_dict(), "./modelCache_{:.0f}.pt".format(ld * 1e7))
+            torch.save(model.state_dict(), "./modelCache_{:.0f}.pt".format(ld * 1e14))
             
             # evaluate model
             eval(model, DEVICE, ts_loader_y)
