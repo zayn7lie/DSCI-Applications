caa22e3c35312bbdf4ed8528fd7458b93d40695f
zayn7lie-win <zayn7lie.ber7+github@gmail.com>
2023-03-08 13:40:54 +0800

    test

--- main.py
+++ main.py
@@ -43,7 +43,7 @@ def main():
         ts_loader_x = DataLoader(fr_dataset, BATCH_SIZE, num_workers=NUM_WORKERS, sampler=fr_ts_idxs)
         ts_loader_y = DataLoader(to_dataset, BATCH_SIZE, num_workers=NUM_WORKERS, sampler=to_ts_idxs)
         # print("K-fold:", fr_idx_9, "+", to_idx_9, "->", to_idx_1)
-        for ld in [1e-7, 0]:
+        for ld in [1e-9, 0]:
             # load model
             print("\n### LAMBDA = {:.0f} * 1e-7\n".format(ld * 1e7))
             model = RMMD()
