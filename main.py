447cce0009e9b84d600006c00cbac9dbe217d5b8
zayn7lie-win <zayn7lie.ber7+github@gmail.com>
2023-03-08 11:17:18 +0800

    test

--- main.py
+++ main.py
@@ -43,9 +43,9 @@ def main():
         ts_loader_x = DataLoader(fr_dataset, BATCH_SIZE, num_workers=NUM_WORKERS, sampler=fr_ts_idxs)
         ts_loader_y = DataLoader(to_dataset, BATCH_SIZE, num_workers=NUM_WORKERS, sampler=to_ts_idxs)
         # print("K-fold:", fr_idx_9, "+", to_idx_9, "->", to_idx_1)
-        for ld in [1e-5, 0]:
+        for ld in [1e-7, 0]:
             # load model
-            print("\n### LAMBDA = {:.4f}\n".format(ld))
+            print("\n### LAMBDA = {:.0f} * 1e-7\n".format(ld * 1e7))
             model = RMMD()
             model.to(DEVICE)
             if os.path.exists("./modelCache.pt"):
