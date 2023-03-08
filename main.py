968cf444d797cdd54981fe3a8e5de8b231656cfc
zayn7lie-win <zayn7lie.ber7+github@gmail.com>
2023-03-09 00:03:16 +0800

    test

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ def main():
     modellr = 1e-4
     BATCH_SIZE = 25
     NUM_WORKERS = 2
-    EPOCHS = 50
+    EPOCHS = 60
     DEVICE = torch.device('cuda' if torch.cuda.is_available() else 'cpu') # xm.xla_device()
     K = 1 # k-fold
     criterion = torch.nn.BCELoss() # BCEFocalLoss()
