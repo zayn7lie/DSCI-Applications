f9c281af3518a6de794f156ebf30875f22b3b986
zayn7lie-win <zayn7lie.ber7+github@gmail.com>
2023-03-13 07:44:15 +0800

    updated

--- main.py
+++ main.py
@@ -43,7 +43,7 @@ def main():
         ts_loader_x = DataLoader(fr_dataset, BATCH_SIZE, num_workers=NUM_WORKERS)#, sampler=fr_ts_idxs)
         ts_loader_y = DataLoader(to_dataset, BATCH_SIZE, num_workers=NUM_WORKERS)#, sampler=to_ts_idxs)
         # print("K-fold:", fr_idx_9, "+", to_idx_9, "->", to_idx_1)
-        for ld in [1e-5, 0, 1e-6, 1e-7, 1e-8, 1e-9]:
+        for ld in [1e-9, 1e-5, 0, 1e-6, 1e-7, 1e-8]:
             # load model
             epochs = EPOCHS
             print("\n### LAMBDA = {:.0f} * 1e-14\n".format(ld * 1e14))
