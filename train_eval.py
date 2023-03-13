71ec444ecf34bd2a18f339564981fe28442eea4b
zayn7lie-win <zayn7lie.ber7+github@gmail.com>
2023-03-13 09:31:23 +0800

    test

--- train_eval.py
+++ train_eval.py
@@ -77,7 +77,7 @@ def eval(model, device, test_loader):
         for i in range(8):
             # print(sum_o[i], sum_t[i])
             # print(np.array(sum_o[i] >= , dtype=float))
-            temp_f1, temp_auc = f1_score(sum_t[i], np.array(sum_o[i] >= 0.01, dtype=float)), roc_auc_score(sum_t[i], sum_o[i])
+            temp_f1, temp_auc = f1_score(sum_t[i], np.array(sum_o[i] >= 0.001, dtype=float)), roc_auc_score(sum_t[i], sum_o[i])
             print("- {:.0f}th: F1: {:.4f}% Auc: {:.4f}%".format(i, temp_f1, temp_auc))
             f1 += temp_f1
             auc += temp_auc
