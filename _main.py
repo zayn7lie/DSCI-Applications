14e9cfa2e7e7e982426a450cfb635983aeff7ffb
zayn7lie-win <zayn7lie.ber7+github@gmail.com>
2023-03-19 07:51:50 +0800

    test

--- _main.py
+++ _main.py
@@ -21,15 +21,15 @@ NUM_WORKERS = 2
 modellr = 1e-4
 times = 100
 param = 0.1
-EPOCHS = 0
+EPOCHS = 60
 
 compare = [1e-6, 0] #lambda
 criterion = torch.nn.BCEWithLogitsLoss() # BCELogitsFocalLoss() # 
 TF = False
-DropBlock = True
+DropBlock = False
 
-fr_dataset = odirData("./OIA-ODIR/Off-site Test Set", TF=TF)
-to_dataset = odirData("./OIA-ODIR/On-site Test Set", TF=TF)
+fr_dataset = odirData("./OIA-ODIR/On-site Test Set", TF=TF)
+to_dataset = odirData("./OIA-ODIR/Off-site Test Set", TF=TF)
 
 def main():
     # K-fold
